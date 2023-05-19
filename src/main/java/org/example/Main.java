862250d91edb3516e32d8ab2008867ae02895a9d
Aliii10 <127393702+Aliii10@users.noreply.github.com>
2023-05-19 10:31:30 +0300

    Автоматизирован вывод спортсменов и расчёт времени

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        int result1 = teamNumber1();
-        int result2 = teamNumber2();
+    public static void main(String[] args) {
+        Team team1 = new Team("Команда 1");
+        Team team2 = new Team("Команда 2");
+        int result1 = teamNumber1(team1);
+        int result2 = teamNumber2(team2);
         if (result1 < result2) {
             System.out.println("Победила первая команда с результатом: " + result1);
         } else if (result1 > result2) {
@@ -16,15 +18,8 @@ public class Main {
             System.out.println("Победила дружба!!! Результаты команд: " + result2);
         }
     }
-    Scanner console = new Scanner(System.in);
-    System.out.print("Введите название первой команды: ");
-    Team team1 = new Team(console.nextLine());
-    System.out.print("Введите название второй команды: ");
-    Team team2 = new Team(console.nextLine());
 
-    int result1 = team1.sumTime();
-    int result2 = team2.sumTime();
-    static int teamNumber1() {
+    static int teamNumber1(Team team) {
         System.out.println("Список спортсменов первой команды: ");
         String text = """
                 1. Иванов Иван Иванович
@@ -34,16 +29,15 @@ public class Main {
                 5. Снежкова Мария Юрьевна
                 """;
         System.out.println(text);
-        System.out.println("Введите время каждого спортсмена первой команды: ");
 
-        int [] time1 = inputTime();
+        team.inputCompetings();
 
-        int summa = sumTime(time1);
+        int summa = team.getSummaTime();
         System.out.println("Сумма общего времени первой команды: " + summa);
         return summa;
     }
 
-    static int teamNumber2() {
+    static int teamNumber2(Team team) {
 
 
         System.out.println("Список спортсменов второй команды: ");
@@ -55,21 +49,13 @@ public class Main {
                 5. Владов Владлен Владиславович
                 """;
         System.out.println(text2);
-        System.out.println("Ввведите время каждого спортсмена второй команды: ");
 
-        int[] time2 = inputTime();
-        int summa = sumTime(time2);
+        team.inputCompetings();
+
+        int summa = team.getSummaTime();
         System.out.println("Сумма общего времени второй команды: " + summa);
         System.out.println();
         return summa;
     }
-
-    public static int[] inputTime() {
-        int[] medals = new int[5];
-        Scanner console = new Scanner(System.in);
-        for (int i = 0; i < medals.length; i++) {
-            medals[i] = console.nextInt();
-        }
-        return medals;
-    }
 }
+
--- src/main/java/org/example/Team.java
+++ src/main/java/org/example/Team.java
@@ -7,40 +7,31 @@ import java.util.Scanner;
 
 public class Team {
     private List<Competing> sportsmens;
-
     private String teamName;
-    private int time;
     //  private List<Competing> competings;
 
     public Team(String teamName) {
         this.teamName = teamName;
         this.sportsmens = new ArrayList<>();
-
     }
 
-    ArrayList<Integer> timeSumm = new ArrayList();
-
-    private void inputCompetings() {
+    public void inputCompetings() {
         Scanner console = new Scanner(System.in);
         System.out.println("Введите время каждого спортсмена команды " + teamName + ":");
 
-        for (int i = 0; i < 5; i++) {
-            System.out.print("Введите имя спортсмена " + (i + 1) + ": ");
-            String name = console.nextLine();
-            System.out.print("Введите время спортсмена " + (i + 1) + ": ");
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Введите время спортсмена " + i + ": ");
             int time = console.nextInt();
-            timeSumm.add(time);
             console.nextLine(); // очистка буфера
-            sportsmens.add(new Competing(name, time));
+            sportsmens.add(new Competing("Спортсмен " + i, time));
         }
     }
 
     public int getSummaTime() {
-
         int result = 0;
 
-        for (int i = 0; i < 5; i++) {
-            result += timeSumm.get(i);
+        for (Competing s : sportsmens) {
+            result += s.getTime();
         }
         return result;
     }
